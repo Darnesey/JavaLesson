95a1ff8869ea299e22eaf1d16dca92795b574fb2
Ryan Darnell <rdarnell1992@gmaill.com>
2016-07-13 16:50:29 -0600

    Solution

--- app/src/main/java/softwarefactory/javalessons/Start.java
+++ app/src/main/java/softwarefactory/javalessons/Start.java
@@ -13,29 +13,7 @@ public class Start {
 
     public static ArrayList<Student> students = new ArrayList<>();
 
-    public static void main(){
-        // Insert code inside this method. This method is called when the button is pressed
-        /*
-        For this lesson, you will need to create a new Class within the same directory as Start.
 
-         */
-
-
-
-
-
-
-        // your code will stop here
-    }
-
-
-    public static void printMenu(){
-        println("Class List Menu");
-        println("Please Enter an option:" +
-                "\n1) Add a new student" +
-                "\n2) Delete a student" +
-                "\n3) Print the class list");
-    }
 
     public static void addStudent(String name){
         Student student = new Student();
